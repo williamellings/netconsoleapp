d16a6d898a5d02a2c8ab161f33851b0850078235
williamellings <wille.ellings@hotmail.com>
2025-09-09 12:44:59 +0200

    asassas

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace netconsoleappövning
     internal class Program
     {
         static void Main(string[] args)
+           
         {
         }
     }
